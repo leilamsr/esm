74920ea81c36ad1a3e02d0da51c723eefa03da96
leila <leila.m2006131@gmail.com>
2025-06-01 21:01:02 +0330

    add Book method

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,22 @@
+import java.util.Queue;
+import java.util.LinkedList;
+
 public class Book {
+    private Queue<String> waitlist;
+
+    public Book() {
+        waitlist = new LinkedList<>();
+    }
+
+    public void add(String member) {
+        waitlist.add(member);
+    }
+
+    public String getNextInWaitlist() {
+        return waitlist.poll();
+    }
+
+    public boolean hasWaitlist() {
+        return !waitlist.isEmpty();
+    }
 }
