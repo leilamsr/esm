96cbd53a038cbefcad3fb74843bbdb6997affc4e
leila <leila.m2006131@gmail.com>
2025-06-01 22:23:16 +0330

    add TransactionManager method

--- src/TransactionManager.java
+++ src/TransactionManager.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TransactionManager {
+    private List<Transaction> transactions;
+
+    public TransactionManager() {
+        transactions = new ArrayList<>();
+    }
+
+    public void addTransaction(Transaction transaction) {
+        transactions.add(transaction);
+    }
+
+    public Transaction getTransactionByIndex(int index) {
+        return transactions.get(index);
+    }
 }
