a87ee302d2ff7a6b427dd19ca4f267646e051eec
leila <leila.m2006131@gmail.com>
2025-06-01 22:46:54 +0330

    add Member method

--- src/Member.java
+++ src/Member.java
@@ -1,2 +1,26 @@
+import java.util.LinkedList;
+
 public class Member {
+    private String memberId;
+    private LinkedList<Transaction> transactions;
+
+    public Member(String memberId) {
+        this.memberId = memberId;
+        transactions = new LinkedList<>();
+    }
+
+    public void addTransaction(Transaction transaction) {
+        transactions.add(transaction);
+    }
+
+    public Transaction getLastTransaction() {
+        if (transactions.isEmpty()) {
+            return null;
+        }
+        return transactions.getLast();
+    }
+
+    public String getMemberId() {
+        return memberId;
+    }
 }
